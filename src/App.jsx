43e9814f3a2ddb401d17d72c84ec24f8762e29d2
Naomi Nosa <naominosa296@gmail.com>
2025-03-17 14:26:28 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Colorpicker from './color'
+
+function App() {
+
+  return (
+    <>
+      <Colorpicker/>
+    </>
+  )
+}
+
+export default App
---
+++ src/color.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+function Colorpicker() {
+    const [color,setColor]=useState("#697565");
+    function handleColorPicker(e){
+        setColor(e.target.value);
+    }
+  return (
+    <div className='color-picker-container'>
+
+      <h1>Color Picker</h1>
+      <div className="color-display" style={{backgroundColor:color}}>
+        <p>Selected Color:{color}</p>
+        <label > select a color:</label>
+        <input type="color" value={color} onChange={handleColorPicker} />
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Colorpicker
